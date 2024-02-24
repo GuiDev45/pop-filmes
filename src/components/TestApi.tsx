98799a8ec43f413aba93cc9ebf4674cc185c9cc4
GuiDev45 <bortolettodev@gmail.com>
2024-02-24 16:20:30 -0300

    Etapa 2 - segundo teste com a API

--- src/components/TestApi.tsx
+++ src/components/TestApi.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 interface Movie {
   id: number;
   title: string;
+  poster_path: string;
 }
 
 type MovieApiResponse = {
@@ -18,13 +19,11 @@ export default function TestApi() {
     const fetchData = async () => {
       try {
         const response = await axios.get<MovieApiResponse>(
-          "https://api.themoviedb.org/3/movie/popular",
+          "https://api.themoviedb.org/3/trending/movie/day",
           {
             params: {
-              api_key: import.meta.env.VITE_API_KEY,
-            },
-            headers: {
-              Authorization: `Bearer ${import.meta.env.VITE_JWT_TOKEN}`,
+              api_key: "308669bdff0df0fa1c1cc92c06f8d1dd",
+              language: "pt-BR", // Definindo o idioma para português do Brasil
             },
           },
         );
@@ -46,7 +45,16 @@ export default function TestApi() {
           <h3>Filmes</h3>
           <ul>
             {data.results.map((movie) => (
-              <li key={movie.id}>{movie.title}</li>
+              <li key={movie.id}>
+                <h4>{movie.title}</h4>
+                {movie.poster_path && (
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                    alt={movie.title}
+                    style={{ width: "200px", height: "300px" }}
+                  />
+                )}
+              </li>
             ))}
           </ul>
         </div>
