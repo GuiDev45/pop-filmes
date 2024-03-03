1e82a2800026058477ea5a839930f426656de2ec
GuiDev45 <bortolettodev@gmail.com>
2024-03-03 14:04:52 -0300

    Atualização Etapa - 4, Ajuste no css header, ajuste nos tipos da API e consumo de API na Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,47 +1,72 @@
 import { FaPlus, FaSearch } from "react-icons/fa";
 import "./style.css";
 import { Link } from "react-router-dom";
+import { useFetchMovies } from "../../services/Api";
+import { Movie } from "../../types/MovieType";
 
 export default function Home() {
-  return (
-    <div className="containerHeader">
-      <div className="navLinks gap-4">
-        <Link to={"/"}>
-          <img src="/public/logo.svg" alt="Logo Pop" />
-        </Link>
-        <Link to={"#"} className="links">
-          Filmes
-        </Link>
-        <Link to={"#"} className="links">
-          Séries
-        </Link>
-        <Link to={"#"} className="links">
-          Pessoas
-        </Link>
-        <Link to={"#"} className="links">
-          Mais
-        </Link>
-      </div>
+  const { data, isLoading, isError } = useFetchMovies(1);
 
-      <div className="navLinks gap-4">
-        <Link to={"#"}>
-          <FaPlus className="iconPlus" />
-        </Link>
-        <Link to={"#"} className="links">
-          Pessoas
-        </Link>
-        <Link to={"#"} className="links">
-          PT
-        </Link>
-        <Link to={"#"} className="links">
-          Entrar
-        </Link>
-        <Link to={"/login"} className="links">
-          Junte-se ao POP
-        </Link>
-        <Link to={"#"}>
-          <FaSearch className="iconSearch" size={20} />
-        </Link>
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error fetching data</div>;
+
+  return (
+    <div>
+      <div className="containerHeader bg">
+        <div className="navLinks">
+          <Link to={"/"} className="iconLogo">
+            <img src="/logo.svg" alt="Logo Pop" />
+          </Link>
+          <Link to={"#"} className="links">
+            Filmes
+          </Link>
+          <Link to={"#"} className="links">
+            Séries
+          </Link>
+          <Link to={"#"} className="links">
+            Pessoas
+          </Link>
+          <Link to={"#"} className="links">
+            Mais
+          </Link>
+        </div>
+
+        <div className="navLinks">
+          <Link to={"#"}>
+            <FaPlus className="iconPlus" />
+          </Link>
+          <Link to={"#"} className="links">
+            Pessoas
+          </Link>
+          <Link to={"#"} className="links">
+            PT
+          </Link>
+          <Link to={"#"} className="links">
+            Entrar
+          </Link>
+          <Link to={"/login"} className="links">
+            Junte-se ao POP
+          </Link>
+          <Link to={"#"}>
+            <FaSearch className="iconSearch" size={20} />
+          </Link>
+        </div>
+      </div>
+      <div className="">
+        {data.results.map((movie: Movie) => (
+          <div key={movie.id}>
+            <h2>{movie.title}</h2>
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              alt={movie.title}
+            />
+            <p>{movie.overview}</p>
+            <p>Popularidade: {movie.popularity}</p>
+            <p>Data de lançamento: {movie.release_date}</p>
+            <p>Contagem de votos: {movie.vote_count}</p>
+            <p>Média de votos: {movie.vote_average}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/types/MovieType.ts
+++ src/types/MovieType.ts
@@ -2,4 +2,9 @@ export type Movie = {
   id: number;
   title: string;
   poster_path: string;
+  overview: string;
+  popularity: number;
+  release_date: string;
+  vote_average: number;
+  vote_count: number;
 };
